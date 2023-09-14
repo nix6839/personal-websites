596a7a0326c81cede2395a7da033f4444f6ffbca
Han Yeong-woo <han@yeongwoo.dev>
2023-09-15 00:01:31 +0900

    feat: define color tokens

--- apps/portfolio/tailwind.config.ts
+++ apps/portfolio/tailwind.config.ts
@@ -1,9 +1,55 @@
+import tailwindColors from 'tailwindcss/colors.js';
+
 import type { Config } from 'tailwindcss';
+
+const COLORS = {
+  ...tailwindColors,
+  brand: '#b4ec94',
+} as const;
 
 const config = {
   content: ['./src/**/*.astro'],
   corePlugins: {
     preflight: false,
+  },
+
+  theme: {
+    backgroundColor: {
+      primary: {
+        DEFAULT: COLORS.zinc['900'],
+      },
+    },
+
+    textColor: {
+      brand: {
+        DEFAULT: COLORS.brand,
+      },
+      primary: {
+        DEFAULT: COLORS.zinc['100'],
+        hover: COLORS.zinc['300'],
+        pressed: COLORS.zinc['400'],
+      },
+      secondary: {
+        DEFAULT: COLORS.zinc['200'],
+      },
+      tertiary: {
+        DEFAULT: COLORS.zinc['300'],
+      },
+    },
+
+    borderColor: {
+      primary: {
+        DEFAULT: COLORS.zinc['500'],
+      },
+    },
+
+    fill: {
+      icon: {
+        primary: {
+          DEFAULT: COLORS.zinc['200'],
+        },
+      },
+    },
   },
 } satisfies Config;
 
