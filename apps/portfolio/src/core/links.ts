5d6266d807c5c29cbdfded1f88ac6661fca3ec77
Han Yeong-woo <han@yeongwoo.dev>
2023-09-17 08:01:56 +0900

    refactor: SSOT for link (#18)

---
+++ apps/portfolio/src/core/links.ts
@@ -0,0 +1,15 @@
+const Links = {
+  root: '/',
+
+  works: '/works',
+  work(slug: string) {
+    return `${this.works}/${slug}` as const;
+  },
+} as const;
+
+export default Links;
+
+export type RootLink = (typeof Links)['root'];
+
+export type WorksLink = (typeof Links)['works'];
+export type WorkLink = ReturnType<(typeof Links)['work']>;
